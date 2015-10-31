f8216c284a06e9aff798e66d1f30970c0d842187
janhajk <janhajk@gmail.com>
2015-10-31 21:17:51 +0000

    update to gmap api v3

--- filter.js
+++ filter.js
@@ -1,9 +1,3 @@
-// Zentrum der Karte
-var dossiersearch_zentrum_name = Drupal.settings.dossiersearch.center.title;
-var dossiersearch_zentrum = new google.maps.LatLng(
-   Drupal.settings.dossiersearch.center.latitude,
-   Drupal.settings.dossiersearch.center.longitude);
-
 var dossiersearch_showmap = Drupal.settings.dossiersearch.gmap.show;
 var dossiersearch_usemap  = Drupal.settings.dossiersearch.gmap.use;
 
@@ -25,10 +19,6 @@ if(Drupal.jsEnabled) {
          //$('#dossiersearch_results table').tablesorter({headers: {1:{sorter:'germandate'}}});
          dossiersearch_addDateFilter();
 
-         if (!dossiersearch_usemap) { $('#dossiersearch_filter_location').hide(); }
-         $('#dossiersearch_filters').fadeIn(400);
-         //$('#dossiersearch_filter_group_select').attr("disabled", true);
-
          // Initial Suche
          dossiersearch_search_init(0);
 
@@ -44,22 +34,7 @@ if(Drupal.jsEnabled) {
            '#dossiersearch_filter_group_select').change(function(){
             dossiersearch_search_init(0);
          });
-         if (dossiersearch_showmap && dossiersearch_usemap) {
-            $('#dossiersearch_gmap').show();
-            gmap_initialize();
-            dossiersearch_displayProjekt();
-            $('#dossiersearch_filter_map_active').attr('checked',false);
-         }
-         $('#dossiersearch_filter_map_active').change(function(){
-            if ($('#dossiersearch_filter_map_active').attr('checked')) {
-               if (firsttime && !dossiersearch_showmap) {
-                  $('#dossiersearch_gmap').show();
-                  gmap_initialize();
-                  dossiersearch_displayProjekt();
-               }
-            }
-            dossiersearch_search_init(0);
-         });
+
          $('#dossiersearch_view img').click(function() {
             var view = dossiersearch_getCurrentView();
             view = (view === 'Normal') ? 'Thumbnails' : 'Normal';
@@ -132,70 +107,6 @@ function dossiersearch_search() {
       $('#dossiersearch_results_body').css('text-align','left').fadeIn(); // Resultate anzeigen
       previews_init();
    });
-   return true;
-}
-
-/*
- * Places Marks on the Map for every Node
- */
-function dossiersearch_nodeMarks(nid,position) {
-   if (dossiersearchMarkers[nid] != undefined) {
-      dossiersearchMarkers[nid].setMap(null);
-   }
-   dossiersearchMarkers[nid] = new google.maps.Marker({
-      position: position,
-      map: dossiersearchMap,
-      icon: '/sites/all/modules/custom/drupal_searchtools/images/bluedot.png'
-   });
-   google.maps.event.addListener(dossiersearchMarkers[nid], 'click', function() {
-      window.location = '/node/' + nid;
-   });
-}
-
-// Gmap darstellen
-function gmap_initialize() {
-   if (!dossiersearch_usemap) return false;
-   firsttime = false;
-   var myOptions = {
-      zoom: 14,
-      center: dossiersearch_zentrum,
-      mapTypeId: google.maps.MapTypeId.HYBRID,
-      navigationControl: true,
-      navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
-      mapTypeControl: false,
-      scaleControl: true,
-      streetViewControl: false
-   }
-
-   dossiersearchMap = new google.maps.Map(document.getElementById("dossiersearch_gmap"), myOptions);
-
-   google.maps.event.addListener(dossiersearchMap, 'bounds_changed', function() {
-      document.getElementById('dossiersearch_filter_bounds').innerHTML = dossiersearchMap.getBounds();
-      if ($('#dossiersearch_filter_map_active').attr('checked')) {
-         dossiersearch_search_init(1500);
-      }
-      dossiersearch_updateExport();
-   });
-
-   // erzeugt einen "Home"-Button
-   var homeControlDiv = document.createElement('DIV');
-   homeControlDiv.style.padding = '5px';
-   var controlUI = document.createElement('DIV');
-   controlUI.id = 'dossiersearch_filter_map_home_button';
-   controlUI.title = 'Hier clicken um zurück nach ' + dossiersearch_zentrum_name + ' zu kommen';
-   homeControlDiv.appendChild(controlUI);
-   var controlText = document.createElement('DIV');
-   controlText.id = 'dossiersearch_filter_map_home_text';
-   controlText.innerHTML = dossiersearch_zentrum_name;
-   controlUI.appendChild(controlText);
-   google.maps.event.addDomListener(controlUI, 'click', function() {
-      dossiersearchMap.panTo(dossiersearch_zentrum);
-      dossiersearchMap.setOptions({zoom:14});
-   });
-   homeControlDiv.index = 1;
-   dossiersearchMap.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
-   // Ende Home-Button
-
    return true;
 }
 
@@ -279,7 +190,7 @@ function dossiersearch_setHeader(view) {
    var header = (view === 'Thumbnails') 
    ? '<tr><th>Titel</th></tr>'
    : '<tr><th>Titel</th><th>letzte &Auml;nderung</th><th>Dok-Datum</th><th>Autor</th><th>Dateien</th><th>Gruppen</th><th>Tags</th></tr>';
-   $('#dossiersearch_results_head').html(header); 
+   $('#dossiersearch_results_head').html(header);
 }
 
 
---
+++ filter.map.js
@@ -0,0 +1,98 @@
+// Zentrum der Karte
+var dossiersearch_zentrum_name = Drupal.settings.dossiersearch.center.title;
+var dossiersearch_zentrum = new google.maps.LatLng(Drupal.settings.dossiersearch.center.latitude, Drupal.settings.dossiersearch.center.longitude);
+
+
+
+if(Drupal.jsEnabled) {
+   $(document).ready(function() {
+
+      if (!dossiersearch_usemap) { $('#dossiersearch_filter_location').hide(); }
+      $('#dossiersearch_filters').fadeIn(400);
+      //$('#dossiersearch_filter_group_select').attr("disabled", true);
+
+      if(dossiersearch_showmap && dossiersearch_usemap) {
+         $('#dossiersearch_gmap').show();
+         gmap_initialize();
+         dossiersearch_displayProjekt();
+         $('#dossiersearch_filter_map_active').attr('checked', false);
+      }
+      $('#dossiersearch_filter_map_active').change(function() {
+         if($('#dossiersearch_filter_map_active').attr('checked')) {
+            if(firsttime && !dossiersearch_showmap) {
+               $('#dossiersearch_gmap').show();
+               gmap_initialize();
+               dossiersearch_displayProjekt();
+            }
+         }
+         dossiersearch_search_init(0);
+      });
+   });
+}
+
+
+
+
+/*
+ * Places Marks on the Map for every Node
+ */
+function dossiersearch_nodeMarks(nid,position) {
+   if (dossiersearchMarkers[nid] != undefined) {
+      dossiersearchMarkers[nid].setMap(null);
+   }
+   dossiersearchMarkers[nid] = new google.maps.Marker({
+      position: position,
+      map: dossiersearchMap,
+      icon: '/sites/all/modules/custom/drupal_searchtools/images/bluedot.png'
+   });
+   google.maps.event.addListener(dossiersearchMarkers[nid], 'click', function() {
+      window.location = '/node/' + nid;
+   });
+}
+
+// Gmap darstellen
+function gmap_initialize() {
+   if (!dossiersearch_usemap) return false;
+   firsttime = false;
+   var myOptions = {
+      zoom: 14,
+      center: dossiersearch_zentrum,
+      mapTypeId: google.maps.MapTypeId.HYBRID,
+      navigationControl: true,
+      navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+      mapTypeControl: false,
+      scaleControl: true,
+      streetViewControl: false
+   }
+
+   dossiersearchMap = new google.maps.Map(document.getElementById("dossiersearch_gmap"), myOptions);
+
+   google.maps.event.addListener(dossiersearchMap, 'bounds_changed', function() {
+      document.getElementById('dossiersearch_filter_bounds').innerHTML = dossiersearchMap.getBounds();
+      if ($('#dossiersearch_filter_map_active').attr('checked')) {
+         dossiersearch_search_init(1500);
+      }
+      dossiersearch_updateExport();
+   });
+
+   // erzeugt einen "Home"-Button
+   var homeControlDiv = document.createElement('DIV');
+   homeControlDiv.style.padding = '5px';
+   var controlUI = document.createElement('DIV');
+   controlUI.id = 'dossiersearch_filter_map_home_button';
+   controlUI.title = 'Hier clicken um zurück nach ' + dossiersearch_zentrum_name + ' zu kommen';
+   homeControlDiv.appendChild(controlUI);
+   var controlText = document.createElement('DIV');
+   controlText.id = 'dossiersearch_filter_map_home_text';
+   controlText.innerHTML = dossiersearch_zentrum_name;
+   controlUI.appendChild(controlText);
+   google.maps.event.addDomListener(controlUI, 'click', function() {
+      dossiersearchMap.panTo(dossiersearch_zentrum);
+      dossiersearchMap.setOptions({zoom:14});
+   });
+   homeControlDiv.index = 1;
+   dossiersearchMap.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
+   // Ende Home-Button
+
+   return true;
+}
